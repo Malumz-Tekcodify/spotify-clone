004e1a9c4f51a796c0abeff0001ef3bd3eb6022a
Brighton Moyo <brightonmoyo97@gmail.com>
2026-01-17 20:01:23 +0200

    Added component templates to the UI

---
+++ app/ui/suggested_section.tsx
@@ -0,0 +1,132 @@
+const Suggested_section = () => {
+  return (
+    <div className="px-6 grid gap-6 mb-8">
+        <section className="px-6 grid gap-6 mb-8">
+            <div className="flex items-center">
+              <div className="flex-1">
+                <h3 className="text-2xl text-white">
+                  <a
+                    className="border-b border-transparent hover:border-white"
+                    href=""
+                    >Made for Jedidiah</a
+                  >
+                </h3>
+              </div>
+              <div>
+                <a
+                  className="text-xs text-gray-100 tracking-widest uppercase hover:underline"
+                  href=""
+                  >See all</a
+                >
+              </div>
+            </div>
+            <div className="grid grid-cols-6 gap-4">
+              <div className="bg-gray-200 rounded-lg p-5">
+                <div className="relative pt-full mb-4">
+                  <img
+                    className="block w-full absolute inset-0"
+                    src="https://picsum.photos/129.webp?random=1"
+                    alt=""
+                  />
+                </div>
+                <div className="text-sm text-white text-line-clamp-1 mb-1 block">
+                  Chinese Lo-FI
+                </div>
+                <div className="relative pb-5">
+                  <span className="text-xs text-gray-100 text-line-clamp-1"
+                    >By @Nike1nike</span
+                  >
+                </div>
+              </div>
+              <div className="bg-gray-200 rounded-lg p-5">
+                <div className="relative pt-full mb-4">
+                  <img
+                    className="block w-full absolute inset-0"
+                    src="https://picsum.photos/129.webp?random=2"
+                    alt=""
+                  />
+                </div>
+                <div className="text-sm text-white text-line-clamp-1 mb-1 block">
+                  FLY
+                </div>
+                <div className="relative pb-5">
+                  <span className="text-xs text-gray-100 text-line-clamp-1"
+                    >Gaho</span
+                  >
+                </div>
+              </div>
+              <div className="bg-gray-200 rounded-lg p-5">
+                <div className="relative pt-full mb-4">
+                  <img
+                    className="block w-full absolute inset-0"
+                    src="https://picsum.photos/129.webp?random=3"
+                    alt=""
+                  />
+                </div>
+                <div className="text-sm text-white text-line-clamp-1 mb-1 block">
+                  Ludwig van Beethoven
+                </div>
+                <div className="relative pb-5">
+                  <span className="text-xs text-gray-100 text-line-clamp-1"
+                    >Artist</span
+                  >
+                </div>
+              </div>
+              <div className="bg-gray-200 rounded-lg p-5">
+                <div className="relative pt-full mb-4">
+                  <img
+                    className="block w-full absolute inset-0"
+                    src="https://picsum.photos/129.webp?random=4"
+                    alt=""
+                  />
+                </div>
+                <div className="text-sm text-white text-line-clamp-1 mb-1 block">
+                  Hotel Del Luna OST
+                </div>
+                <div className="relative pb-5">
+                  <span className="text-xs text-gray-100 text-line-clamp-1"
+                    >By Jamie Lee</span
+                  >
+                </div>
+              </div>
+              <div className="bg-gray-200 rounded-lg p-5">
+                <div className="relative pt-full mb-4">
+                  <img
+                    className="block w-full absolute inset-0"
+                    src="https://picsum.photos/129.webp?random=5"
+                    alt=""
+                  />
+                </div>
+                <div className="text-sm text-white text-line-clamp-1 mb-1 block">
+                  ITAEWON className (Original Television Soundtrack) Pt.2
+                </div>
+                <div className="relative pb-5">
+                  <span className="text-xs text-gray-100 text-line-clamp-1"
+                    >Gaho</span
+                  >
+                </div>
+              </div>
+              <div className="bg-gray-200 rounded-lg p-5">
+                <div className="relative pt-full mb-4">
+                  <img
+                    className="block w-full absolute inset-0"
+                    src="https://picsum.photos/129.webp?random=6"
+                    alt=""
+                  />
+                </div>
+                <div className="text-sm text-white text-line-clamp-1 mb-1 block">
+                  Preparation For a Journey
+                </div>
+                <div className="relative pb-5">
+                  <span className="text-xs text-gray-100 text-line-clamp-1"
+                    >Gaho</span
+                  >
+                </div>
+              </div>
+            </div>
+          </section>
+    </div>
+  )
+}
+
+export default Suggested_section
